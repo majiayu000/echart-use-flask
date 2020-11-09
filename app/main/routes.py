d8d3429676dee6e81f3a7a405d0248355989a50e
lif <mylifcc@gmail.com>
2020-11-09 16:29:14 +0800

    upright table

--- app/main/routes.py
+++ app/main/routes.py
@@ -58,3 +58,21 @@ def exce():
     #cursor.rowfactory = lambda *args:dict(zip(columns, args))
     data = cursor.fetchall()
     return render_template('exce.html', data=data)
+
+
+@bp.route('/excet/', methods=['GET', 'POST'])
+def excet():
+    connection = cx_Oracle.Connection("hos/hos@192.168.15.50:1521/payy")
+    cursor = connection.cursor()
+    try:
+        cursor.execute("select * from wx_register")
+# except cx_Oracle.DatabaseError as exc:
+    except Exception as err:
+        print("Whoops!")
+        print(err)
+# print(cursor.description)
+    #cursor.execute("select id,idcard from wx_register")
+    #columns = [col[0] for col in cursor.description]
+    #cursor.rowfactory = lambda *args:dict(zip(columns, args))
+    data = cursor.fetchall()
+    return render_template('excet.html', data=data)
